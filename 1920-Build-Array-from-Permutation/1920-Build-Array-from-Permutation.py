8219f86e117dfa69e5f4e54417338efd9fa1174c
Peaky_001 <shreyashdeshwal@gmail.com>
2024-01-02 19:37:58 +0530

    Runtime:93ms Memory:17.45MB

---
+++ 1920-Build-Array-from-Permutation/1920-Build-Array-from-Permutation.py
@@ -0,0 +1,6 @@
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        new_list = []
+        for i in nums:
+            new_list.append(nums[i])
+        return new_list
