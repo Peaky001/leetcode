e85252f1297060597c595f0bc067cd40e4682472
Peaky_001 <shreyashdeshwal@gmail.com>
2024-01-20 01:41:15 +0530

    done some changes

--- 342-power-of-four/342-power-of-four.py
+++ 342-power-of-four/342-power-of-four.py
@@ -1,6 +1,6 @@
 import math
 class Solution:
-    def isPowerOfFour(self, n: int) -> bool:
+    def iisPowerOfFour(self, n: int) -> bool:
         
         if(n>0):
             return math.log(n,4).is_integer()
