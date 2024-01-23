959adf58116f62228cd13e5b5e486e125303a1a4
Peaky_001 <shreyashdeshwal@gmail.com>
2024-01-24 02:03:21 +0530

    Runtime:48ms Memory:17.44MB

---
+++ 242-Valid-Anagram/242-Valid-Anagram.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sorted_s = sorted(s)
+        sorted_t = sorted(t)
+        if sorted_s == sorted_t:
+            return True
+        else:
+            return False
