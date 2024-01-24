8cead674e0b81834e4ceb54a5b46e97abae97e4e
Peaky_001 <shreyashdeshwal@gmail.com>
2024-01-24 21:11:46 +0530

    Runtime:43ms Memory:17.68MB

---
+++ 412-Fizz-Buzz/412-Fizz-Buzz.py
@@ -0,0 +1,15 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        final_list = []
+        for i in range(1,n+1):
+            if i%3==0 and i%5==0:
+                final_list.append("FizzBuzz")
+            elif i%3==0:
+                final_list.append("Fizz")
+            elif i%5==0:
+                final_list.append("Buzz")
+            else:
+                final_list.append(str(i))
+        return final_list
+
+        
